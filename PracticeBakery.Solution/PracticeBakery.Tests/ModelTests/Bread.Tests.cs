42624fad0066cbf0877c95e4d732c89db32dea77
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-06-30 20:46:00 -0700

    populate files with outline/basic elements

---
+++ PracticeBakery.Solution/PracticeBakery.Tests/ModelTests/Bread.Tests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+using PracticeBakery.Models;
+
+namespace PracticeBakery.Tests
+{
+  [TestClass]
+  public class BreadTests
+  {
+  
+  }
+}
---
+++ PracticeBakery.Solution/PracticeBakery/Models/Bread.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System;
+using PracticeBakery.Models;
+
+namespace PracticeBakery.Models
+{
+  public class Bread
+  {
+    // auto implemented properties
+
+    // constructor
+
+    // methods
+  }
+}

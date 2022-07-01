ec4bd11d2e9eaae9334ab02f5f15b72358b45569
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-07-01 14:46:34 -0700

    add basici Program.cs functionality for BreadOrders

--- PracticeBakery.Solution/PracticeBakery/Program.cs
+++ PracticeBakery.Solution/PracticeBakery/Program.cs
@@ -10,8 +10,14 @@ namespace PracticeBakery.Models
     {
       Console.WriteLine("Welcome to Patty's Practice Bakery!");
       Console.WriteLine("------------------MENU------------------");
-      Console.WriteLine("Bread --- $5/each or buy 2 get 1 free");
-      Console.WriteLine("Pastry -- $2/each or buy 3 for $5");
+      Console.WriteLine("Bread --- $5/each *buy 2 get 1 free");
+      Console.WriteLine("Pastry -- $2/each *buy 3 for $5");
+      Console.WriteLine("----------------------------------------");
+      Console.WriteLine("How many breads would you like to order?");
+      int currentOrderedBread = Convert.ToInt32(Console.ReadLine());
+      BreadOrder currentBreadOrder = new BreadOrder(currentOrderedBread);
+      int currentBreadCost = currentBreadOrder.BreadCost();
+      Console.WriteLine(currentOrderedBread + " breads will cost " + currentBreadCost + " dollars.");
     }
   }
 }

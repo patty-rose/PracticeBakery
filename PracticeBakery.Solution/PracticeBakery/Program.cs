17faa139c337019fd5fcd4f5f97e6824abe93c13
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-07-01 14:35:30 -0700

    add basic Program.cs file w/ welcome message

---
+++ PracticeBakery.Solution/PracticeBakery/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using PracticeBakery.Models;
+
+namespace PracticeBakery.Models
+{
+  public class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("Welcome to Patty's Practice Bakery!");
+      Console.WriteLine("------------------MENU------------------");
+      Console.WriteLine("Bread --- $5/each or buy 2 get 1 free");
+      Console.WriteLine("Pastry -- $2/each or buy 3 for $5");
+    }
+  }
+}

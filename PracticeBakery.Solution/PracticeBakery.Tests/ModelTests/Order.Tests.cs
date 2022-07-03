fd74e6f63fc19be7677b52fd72ed62a6923f1887
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-07-02 20:34:50 -0700

    add CalculateOrderTotal method test

--- PracticeBakery.Solution/PracticeBakery.Tests/ModelTests/Order.Tests.cs
+++ PracticeBakery.Solution/PracticeBakery.Tests/ModelTests/Order.Tests.cs
@@ -14,5 +14,14 @@ namespace PracticeBakery.Tests
       Order newOrder = new Order();
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+
+    [TestMethod]
+    public void CalculateOrderTotal_CalculatesOrderTotal_15()
+    {
+      Order newOrder = new Order();
+      newOrder.BreadOrder = new BreadOrder(3);
+      newOrder.PastryOrder = new PastryOrder(3);
+      Assert.AreEqual(15, newOrder.CalculateOrderTotal());
+    }
   }
 }

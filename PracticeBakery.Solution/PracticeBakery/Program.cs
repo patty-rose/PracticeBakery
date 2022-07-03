c02b4333cff2f6ba7dac49839f1af3a570e8d1d0
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-07-02 19:22:53 -0700

    add pastry ordering functionality to program.cs

--- PracticeBakery.Solution/PracticeBakery/Program.cs
+++ PracticeBakery.Solution/PracticeBakery/Program.cs
@@ -8,16 +8,34 @@ namespace PracticeBakery.Models
   {
     static void Main()
     {
+      Console.WriteLine("---------------------------");
+      Console.WriteLine("---------------------------");
       Console.WriteLine("Welcome to Patty's Practice Bakery!");
       Console.WriteLine("------------------MENU------------------");
       Console.WriteLine("Bread --- $5/each *buy 2 get 1 free");
       Console.WriteLine("Pastry -- $2/each *buy 3 for $5");
       Console.WriteLine("----------------------------------------");
       Console.WriteLine("How many breads would you like to order?");
-      int currentOrderedBread = Convert.ToInt32(Console.ReadLine());
-      BreadOrder currentBreadOrder = new BreadOrder(currentOrderedBread);
+      int userInputBread = Convert.ToInt32(Console.ReadLine());
+      BreadOrder currentBreadOrder = new BreadOrder(userInputBread);
       int currentBreadCost = currentBreadOrder.BreadCost();
-      Console.WriteLine(currentOrderedBread + " breads will cost " + currentBreadCost + " dollars.");
+      Console.WriteLine(userInputBread + " breads will cost " + currentBreadCost + " dollars.");
+      Console.WriteLine("How many pastries would you like to order?");
+      int userInputPastry = Convert.ToInt32(Console.ReadLine());
+      PastryOrder currentPastryOrder = new PastryOrder(userInputPastry);
+      int currentPastryCost = currentPastryOrder.PastryCost();
+      Console.WriteLine(userInputPastry + " pastries will cost " + currentPastryCost + " dollars.");
+      Console.WriteLine("---------------------------");
+      Console.WriteLine("----Your-Receipt-From------");
+      Console.WriteLine("--Patty's-Practice-Bakery--");
+      Console.WriteLine("---------------------------");
+      Console.WriteLine("---------------------------");
+      Console.WriteLine(userInputBread + " breads --------------$" + currentBreadCost);
+      Console.WriteLine(userInputPastry + " pastries ------------$" + currentPastryCost);
+      Console.WriteLine("------------------Total:$" + (currentBreadCost + currentPastryCost));
+      Console.WriteLine("---------------------------");
+      Console.WriteLine("-----Have-A-Nice-Day!------");
+      Console.WriteLine("---------------------------");
     }
   }
 }

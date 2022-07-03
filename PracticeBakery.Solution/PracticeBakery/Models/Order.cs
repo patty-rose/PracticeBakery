b532c64a302c1ce15e0f3bc4458942adfcaccc00
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-07-02 19:54:22 -0700

    add and pass Order class constructor test

---
+++ PracticeBakery.Solution/PracticeBakery/Models/Order.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System;
+using PracticeBakery.Models;
+
+namespace PracticeBakery.Models
+{
+  public class Order
+  {
+    // auto implemented properties
+
+    // constructor
+    // public BreadOrder()
+    // {
+    //   BreadOrder breadOrder;
+    //   PastryOrder pastryOrder;
+    // }
+
+    // methods
+  }
+}
